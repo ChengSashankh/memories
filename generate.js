6305782af15fdb314b0db47fb2574030cf7e6f0c
Sashankh <cksashankh@gmail.com>
2018-12-15 16:06:31 +0800

    implement image upload and styling
     * improve style sheet
     * add single and multi image upload support

--- generate.js
+++ generate.js
@@ -1,7 +1,7 @@
 let contentCount = 0;
 
-let setHeading = function (headingText) {
-    document.getElementById('heading').innerHTML = headingText;
+let setHeading = function () {
+    document.getElementById('heading').innerHTML = document.getElementById('headingInput').value;
 }
 
 let createContentArea = function() {
@@ -20,31 +20,91 @@ let createContentArea = function() {
     textSpace.setAttribute('class', 'text_space');
     // textSpace.innerHTML = 'Hello world text';
 
+    let removeButton = document.createElement('button');
+    removeButton.innerHTML = '&times';
+    removeButton.style.borderRadius = '10px';
+    removeButton.style.borderColor = 'black';
+    removeButton.style.borderStyle = 'solid';
+    removeButton.style.borderWidth = '1px';
+    removeButton.style.margin = '0.5%';
+    removeButton.style.backgroundColor = 'Transparent';
+
+    removeButton.setAttribute('id', 'remove');
+    // removeButton.setAttribute('style', 'remove_button');
+    removeButton.onclick = function () {
+        let toRemove = this.parentNode.id.split('_')[1];
+        document.getElementById('content_' + toRemove).style.display = 'none';
+    }
+
+    areaElement.appendChild(removeButton);
     areaElement.appendChild(imageSpace);
     areaElement.appendChild(textSpace);
 
     let rowElement = undefined;
-    if (contentCount % 3 === 0) {
-        rowElement = document.createElement('div');
-        rowElement.setAttribute('id', 'row_' + parseInt(contentCount / 3));
-        rowElement.setAttribute('class', 'row');
-        rowElement.appendChild(areaElement);
-        if (contentCount !== 0)
-            document.getElementById('content-container').appendChild(document.createElement('hr'));
-        document.getElementById('content-container').appendChild(rowElement);
-    } else {
-        rowElement = document.getElementById('row_' + parseInt(contentCount / 3));
-        rowElement.appendChild(areaElement);
-    }
+    // if (contentCount % 3 === 0) {
+    //     rowElement = document.createElement('div');
+    //     rowElement.setAttribute('id', 'row_' + parseInt(contentCount / 3));
+    //     rowElement.setAttribute('class', 'row');
+    //     rowElement.appendChild(areaElement);
+    //     if (contentCount !== 0)
+    //         document.getElementById('content-container').appendChild(document.createElement('hr'));
+    //     document.getElementById('content-container').appendChild(rowElement);
+    // } else {
+        // rowElement = document.getElementById('row_' + parseInt(contentCount / 3));
+        // rowElement.appendChild(areaElement);
+        document.getElementById('content-container').appendChild(areaElement);
+    // }
     contentCount++;
 
-    return 'content_' + (contentCount - 1);
+    return (contentCount - 1);
 }
 
 let hideInputSection = function () {
     document.getElementById('inputSection').style.display = 'none';
+    document.getElementById('addButton').style.display = 'none';
+    
+    document.querySelectorAll('#remove').forEach(e => {
+        e.style.display = 'none';
+    });
+
+    document.getElementById('showMenu').style.display = 'block';
 }
 
-let renderImages = function (files) {
-    files.forE
+let renderImage = function (inputSourceFile) {
+    let sourceFile = inputSourceFile;
+    let reader = new FileReader();
+    let resourceNumber = createContentArea();
+    let target = document.getElementById('imageSpace_' + resourceNumber);
+
+    reader.addEventListener("load", function (f) {
+        target.src = reader.result;
+    }, false);
+
+    if (sourceFile)
+        reader.readAsDataURL(sourceFile);
+}
+
+let showEditMenu = function () {
+    document.getElementById('inputSection').style.display = 'block';
+    document.getElementById('addButton').style.display = 'flex';
+    
+    document.querySelectorAll('#remove').forEach(e => {
+        e.style.display = 'block';
+    });
+
+    document.getElementById('showMenu').style.display = 'none';
+}
+
+let createPreview = function () {
+    setHeading();
+    document.body.style.backgroundColor=document.getElementById('colorChoice').value;
+    let inputFiles = document.getElementById('fileInput').files;
+
+    Array.from(inputFiles).forEach(f => {
+        renderImage(f);
+    });
+}
+
+window.onload = function () {
+    document.getElementById('showMenu').style.display = 'none';
 }
